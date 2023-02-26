191f2873bd3846f488784a4118c2073cac536a6d
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2023-02-27 09:52:55 +1300

    Add files via upload

--- 04_how_much_function_v1.py
+++ 04_how_much_function_v1.py
@@ -1,53 +1,35 @@
 # Functions go here
 def num_checker(question, low, high):
+    
     error_1 = "Please enter a number that's more than 0\n"
     error_2 = "Please enter a number that's less than (or equal to) 10\n"
     error_3 = "Please enter a whole number (without decimal point)\n"
-
+    
     keep_going = ""
     while keep_going == "":
         try:
             # Ask the question
-            response = int(input("How much would you like to play with? "))
+            response = int(input(question))
 
             # If the amount is too low / too high give
-            if  0 < response <= 10:
-                print("You have asked to play with ${}\n".format(response))
+            if  low < response <= high:
+                return response
+                keep_going = "x"
 
             # Output error
-            elif response < 1:
+            elif response < low:
                 print(error_1)
 
-            elif response > 10:
+            elif response > high:
                 print(error_2)
-            
+        
         except ValueError:
             print(error_3)
 
 
 # Main Routine goes here
 
-error_1 = "Please enter a number that's more than 0\n"
-error_2 = "Please enter a number that's less than (or equal to) 10\n"
-error_3 = "Please enter a whole number (without decimal point)\n"
+how_much = num_checker("How much do you want to play with? ", 1, 10)
 
-keep_going = ""
-while keep_going == "":
-    try:
-        # Ask the question
-        response = int(input("How much would you like to play with? "))
-
-        # If the amount is too low / too high give
-        if  0 < response <= 10:
-            print("You have asked to play with ${}\n".format(response))
-
-        # Output error
-        elif response < 1:
-            print(error_1)
-
-        elif response > 10:
-            print(error_2)
-        
-    except ValueError:
-        print(error_3)
-
+print("You have asked to play with ${}".format(how_much))
+print()
